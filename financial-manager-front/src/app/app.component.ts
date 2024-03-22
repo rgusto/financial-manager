57a58bdf04ebf694cfe593cd1489f7a5c38c6a17
Ricardo Moreira <rgusto@gmail.com>
2024-03-22 18:49:45 -0300

    Configurações iniciais, autenticação e entidades usuário e conta

--- financial-manager-front/src/app/app.component.ts
+++ financial-manager-front/src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'financial-manager';
+  sidebar = document.getElementById('mySidebar')!;
+  main = document.getElementById('main')!;
+
+  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+  openNav(): void {
+    if (this.sidebar !== null && this.main !== null) {
+      this.sidebar.style.width = '250px';
+      this.main.style.marginLeft = '250px';
+    }
+  }
+
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  closeNav(): void {
+    if (this.sidebar !== null && this.main !== null) {
+      this.sidebar.style.width = '0';
+      this.main.style.marginLeft = '0';
+    }
+  }
 }
--- financial-manager-front/src/app/app.config.ts
+++ financial-manager-front/src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { APP_ROUTES } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(APP_ROUTES), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
