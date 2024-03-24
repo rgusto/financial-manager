e372cee09118e205045ef627076bdaf5835cb125
Ricardo Moreira <rgusto@gmail.com>
2024-03-24 18:55:30 -0300

    Menu V2

--- financial-manager-front/src/assets/js/menu.js
+++ financial-manager-front/src/assets/js/menu.js
@@ -1,23 +1,40 @@
 const body = document.querySelector("body"),
   sidebar = body.querySelector(".sidebar"),
-  toggle = body.querySelector(".toggle"),
+  sidebarToggle = body.querySelector(".sidebar-toggle"),
   searchBtn = body.querySelector(".search-box"),
-  modeSwitch = body.querySelector(".toggle-switch"),
+  modeSwitch = body.querySelector(".mode-toggle-switch"),
   modeText = body.querySelector(".mode-text");
 
-  toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-  });
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.toggle("dark");
+}
 
-  searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
-  });
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+  sidebar.classList.toggle("close");
+}
 
-  modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    if (body.classList.contains("dark")) {
-      modeText.innerText = "Claro";
-    } else {
-      modeText.innerText = "Escuro";
-    }
-  });
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+  if (sidebar.classList.contains("close")) {
+    localStorage.setItem("status", "close");
+  } else {
+    localStorage.setItem("status", "open");
+  }
+});
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Claro";
+    localStorage.setItem("mode", "dark");
+  } else {
+    modeText.innerText = "Escuro";
+    localStorage.setItem("mode", "light");
+  }
+});
+
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
